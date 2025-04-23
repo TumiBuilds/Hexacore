0c221a0c15aa6c314ff94f1fa4ddd771fc586d22
TumiBuilds <tumelondaba598@gmail.com>
2025-04-23 21:42:12 +0200

    Update Bus.java

--- GroupWork/Bus.java
+++ GroupWork/Bus.java
@@ -24,7 +24,6 @@ public class Bus{
         if (currentPassengers >= capacity){
             throw new OverCapacityException("Bus is full!No more passengers allowed.");
         }
-    //increment
         currentPassengers ++;
     }
     @Override
