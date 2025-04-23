b5b352ae368388c55d31a82d4f4e0d02021a8132
202406415 <salsa123.angel@gmail.com>
2025-04-23 17:24:18 +0200

    Add comments to Sorter.java

--- GroupWork/Sorter.java
+++ GroupWork/Sorter.java
@@ -2,27 +2,31 @@ import java.util.Arrays;
 
 public class Sorter{
 
+    // Selection sort: sorts buses by capacity in ascending order
     public static void selectionSort(Bus[] buses){
         int n = buses.length;
         for(int i = 0; i < n - 1; i++){
             int minIndex = i;
+            // Find the bus with the smallest capacity in the remaining array
          for(int j = i + 1; j < n; j++){
             if(buses[i].getCapacity() < buses[minIndex].getCapacity()){
                minIndex = j; 
             }
          }
-         //Swap
+         //Swap current bus with the smallest one found
          Bus temp = buses[minIndex];
          buses[minIndex] = buses[i];
          buses[i] = temp;
         }
     }
 
+    //Insertion sort: sorts buses by current passengers in ascending order
     public static void insertionSort(Bus[] buses){
         int n= buses.length;
         for(int i = 1; i < n; i++){
             Bus key =  buses[i];
             int j = i - 1;
+            // Insert the current bus in its correct position
             while(j >= 0 && buses[j].getCurrentPassengers()>key.getCurrentPassengers()){
                 buses[j + 1] = buses[j];
                 j--;
