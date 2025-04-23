71bfbf4a50dd2c8e26f97b76955a9ec0c5391037
202406415 <salsa123.angel@gmail.com>
2025-04-23 17:14:47 +0200

    Add comments to Searcher.java

--- GroupWork/Searcher.java
+++ GroupWork/Searcher.java
@@ -1,19 +1,21 @@
 public class Searcher{
     
+    //Linear search by route number
     public static int linearSearch(Bus[] buses, String routeNumber){
         for(int i = 0; i < buses.length; i++){
             if(buses[i].getRouteNumber().equals(routeNumber)){
-                return i;
+                return i;//Found
             }
         }
-        return  -1;
+        return  -1;// Not found
     }
+    //Binary search by capacity(array must be sorted)
     public static int binarySearch(Bus[] buses, int targetCapacity){
         int left = 0, right = buses.length -1;
         while(left <= right){
             int mid = left + (right - left)/2;
             if (buses[mid].getCapacity() == targetCapacity){
-                return mid;
+                return mid;// Found
             }
             if (buses[mid].getCapacity() < targetCapacity){
                 return mid + 1;
@@ -21,6 +23,6 @@ public class Searcher{
                 right = mid - 1;
             }
         }
-        return -1;
+        return -1;// Not found
     } 
 }
