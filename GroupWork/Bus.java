3e73c0934f775b1fd23236a6754ba5b24c6a86d7
Max <nginanie@gmail.com>
2025-04-25 09:44:11 +0200

    Update Bus.java

--- GroupWork/Bus.java
+++ GroupWork/Bus.java
@@ -1,15 +1,18 @@
 public class Bus{
-    // fields
+    
+    // bus fields
     private String routeNumber;
     private int capacity;
     private int currentPassengers;
-    // constructor to set the fields
+    
+    // constructor to set the bus fields
     public Bus(String routeNumber ,int capacity){
         this.routeNumber =routeNumber;
         this.capacity = capacity;
         this.currentPassengers = 0;
     }
-    // getters to access the private fields
+    
+    // getters to access the private bus fields
     public String getRouteNumber(){
         return routeNumber;
     }
@@ -19,19 +22,24 @@ public class Bus{
     public int getCurrentPassengers(){
         return currentPassengers;
     }
+    
     //method adds passengers
+    
     public void addPassenger() throws OverCapacityException {
         if (currentPassengers >= capacity){
             throw new OverCapacityException("Bus is full!No more passengers allowed.");
         }
         currentPassengers ++;
     }
+    // displays bus fields
+    
     @Override
     public String toString(){
         return "Bus Route: "+routeNumber+ ", Capacity: "+capacity+ ", Passengers: "+currentPassengers;
     }
 }
-//inheritance
+//inherits the exception class 
+
  class OverCapacityException extends Exception {
 
     public OverCapacityException(String message){
