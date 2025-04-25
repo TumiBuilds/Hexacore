3a0908530d910d3cf10dc13304f9aff1816993ac
Max <nginanie@gmail.com>
2025-04-25 09:39:21 +0200

    Update MainApp.java

--- GroupWork/MainApp.java
+++ GroupWork/MainApp.java
@@ -1,16 +1,19 @@
 public class MainApp{
     public static void main(String[] args){
+        //array bus storing 3 objects
         Bus[] buses = { 
             new Bus("M091" ,50),
             new Bus("N803" ,30),
             new Bus("A200" ,60)
         };
-             //Sorting by capacity
+        //capacity before sorting
         System.out.println("Before Sorting: ");
         for(Bus b : buses) System.out.println(b);
-
+        
+         //sorter
         Sorter.selectionSort(buses);
-
+        
+          // capacity after sorting
         System.out.println("\nAfter Sorting by Capacity: ");
         for(Bus b : buses) System.out.println(b);
  
@@ -18,7 +21,8 @@ public class MainApp{
          String searchRoute = "M091";
          int index = Searcher.linearSearch(buses, searchRoute);
          System.out.println("\nBus found at index: " +index);
-
+        
+           //exception handling 
          try {
             buses[1].addPassenger();
             System.out.println("\nAdded passenger to: " + buses[1]);
