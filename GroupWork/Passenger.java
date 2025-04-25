d5417a027c4b266cca6bebcc860d4bd014ddb25e
Max <nginanie@gmail.com>
2025-04-25 09:35:12 +0200

    Update Passenger.java

--- GroupWork/Passenger.java
+++ GroupWork/Passenger.java
@@ -1,14 +1,15 @@
 public class Passenger{
+    //passenger fields 
     private String name;
     private String ticketNumber;
     private String destination;
-
+    // constructor to set passenger fields
     public Passenger(String name ,String ticketNumber ,String destination){
         this.name = name;
         this.ticketNumber = ticketNumber;
         this.destination = destination;
     }
-
+   // getters to access private passenger fields 
     public String getName(){
         return name;
     }
@@ -18,6 +19,8 @@ public class Passenger{
     public String getDestination(){
         return destination;
     }
+
+    // displays 
     public String toString(){
         return "Passenger: "+name+ ", Tickect: "+ticketNumber+ ", Destination: "+destination;
     }
